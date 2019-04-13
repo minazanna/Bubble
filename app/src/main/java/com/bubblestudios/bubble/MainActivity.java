937ea1dffc8b0a14fd656e1a655c9f8190753d43
Milan Parikh <milanmp98@gmail.com>
2019-04-13 13:01:20 -0400

    Added upload activity to facilitate adding songs

--- app/src/main/java/com/bubblestudios/bubble/MainActivity.java
+++ app/src/main/java/com/bubblestudios/bubble/MainActivity.java
@@ -1,8 +1,12 @@
 package com.bubblestudios.bubble;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements CardsFragment.OnFragmentInteractionListener {
 
@@ -16,4 +20,33 @@ public class MainActivity extends AppCompatActivity implements CardsFragment.OnF
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_settings_button:
+                //settings menu
+                return true;
+            case R.id.menu_upload_button:
+                Intent intent = new Intent(this, UploadActivity.class);
+                startActivity(intent);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
---
+++ app/src/main/java/com/bubblestudios/bubble/UploadActivity.java
@@ -0,0 +1,202 @@
+package com.bubblestudios.bubble;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.net.Uri;
+import android.provider.OpenableColumns;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class UploadActivity extends AppCompatActivity {
+
+    private int PICK_IMAGE_REQUEST = 1;
+    private int PICK_AUDIO_REQUEST = 2;
+    private Uri albumArtFilePath;
+    private Uri snippetFilePath;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
+    private DatabaseReference database;
+    private Button uploadAlbumArtButton, uploadSnippetButton, chooseAlbumArtButton, chooseSnippetButton;
+    private EditText songTitleEditText, artistNameEditText;
+    private String albumArtFileName, snippetFileName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        database = FirebaseDatabase.getInstance().getReference();
+
+        songTitleEditText = findViewById(R.id.song_title_editText);
+        artistNameEditText = findViewById(R.id.artist_name_editText);
+        chooseAlbumArtButton = findViewById(R.id.choose_album_art_button);
+        chooseAlbumArtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseAlbumArt();
+            }
+        });
+        uploadAlbumArtButton = findViewById(R.id.upload_album_art_button);
+        uploadAlbumArtButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadAlbumArt();
+            }
+        });
+
+        chooseSnippetButton = findViewById(R.id.choose_snippet_button);
+        chooseSnippetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseSnippet();
+            }
+        });
+        uploadSnippetButton = findViewById(R.id.upload_snippet_button);
+        uploadSnippetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadSnippet();
+            }
+        });
+        final Button submitButton = findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String songTitle = songTitleEditText.getText().toString();
+                String artistName = artistNameEditText.getText().toString();
+                Snippet snippet = new Snippet(songTitle, artistName, snippetFileName, albumArtFileName);
+                DatabaseReference snippetsRef = database.child("snippets").push();
+                snippetsRef.setValue(snippet).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        chooseAlbumArtButton.setTextColor(Color.BLACK);
+                        chooseSnippetButton.setTextColor(Color.BLACK);
+                        uploadAlbumArtButton.setTextColor(Color.BLACK);
+                        uploadSnippetButton.setTextColor(Color.BLACK);
+                        chooseAlbumArtButton.setText(R.string.choose_album_art);
+                        chooseSnippetButton.setText(R.string.choose_snippet);
+                        uploadAlbumArtButton.setText(R.string.upload);
+                        uploadSnippetButton.setText(R.string.upload);
+                        uploadAlbumArtButton.setEnabled(false);
+                        uploadSnippetButton.setEnabled(false);
+                        artistNameEditText.setText("");
+                        songTitleEditText.setText("");
+                        Toast.makeText(getApplicationContext(), "Submitted Successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+                && data != null && data.getData() != null ) {
+            albumArtFilePath = data.getData();
+            albumArtFileName = getFileName(albumArtFilePath);
+            chooseAlbumArtButton.setTextColor(Color.GREEN);
+            uploadAlbumArtButton.setEnabled(true);
+        } else if (requestCode == PICK_AUDIO_REQUEST && resultCode == Activity.RESULT_OK
+                && data != null && data.getData() != null) {
+            snippetFilePath = data.getData();
+            snippetFileName = getFileName(snippetFilePath);
+            chooseSnippetButton.setTextColor(Color.GREEN);
+            uploadSnippetButton.setEnabled(true);
+        }
+    }
+
+    private void chooseAlbumArt() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    private void chooseSnippet() {
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Song Snippet"), PICK_AUDIO_REQUEST);
+    }
+
+    private void uploadAlbumArt() {
+        if(albumArtFilePath != null) {
+            uploadAlbumArtButton.setText(R.string.uploading);
+            StorageReference ref = storageReference.child("AlbumArt/" + albumArtFileName);
+            ref.putFile(albumArtFilePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    uploadAlbumArtButton.setText(R.string.upload_successful);
+                    uploadAlbumArtButton.setTextColor(Color.GREEN);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    uploadAlbumArtButton.setText(R.string.upload_failed);
+                    uploadAlbumArtButton.setTextColor(Color.RED);
+                }
+            });
+        }
+    }
+
+    private void uploadSnippet() {
+        if(snippetFilePath != null) {
+            uploadSnippetButton.setText(R.string.uploading);
+            StorageReference ref = storageReference.child("Snippets/" + snippetFileName);
+            ref.putFile(snippetFilePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    uploadSnippetButton.setText(R.string.upload_successful);
+                    uploadSnippetButton.setTextColor(Color.GREEN);
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    uploadSnippetButton.setText(R.string.upload_failed);
+                    uploadSnippetButton.setTextColor(Color.RED);
+                }
+            });
+        }
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+}
